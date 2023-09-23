adb8875b45c34a021e9cbb3cff57a5063f2cbaa0
Leif Messinger <leifmessinger@gmail.com>
2023-09-23 08:49:14 -0500

    Django stuff

--- app/Website/wsgi.py
+++ app/Website/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for AttendanceTaker project.
+WSGI config for Website project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AttendanceTaker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Website.settings')
 
 application = get_wsgi_application()
