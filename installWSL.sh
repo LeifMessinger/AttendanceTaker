cf1e3fccc85dacd1beffc55633d29d4dbe54b8c4
Leif Messinger <leifmessinger@gmail.com>
2023-11-05 16:17:31 -0600

    init database in install script

--- installWSL.sh
+++ installWSL.sh
@@ -9,10 +9,13 @@ wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
 popd
 
-cd ./app
+pushd ./app
 pip3 install -r requirements.txt
 
 sudo pip3 install pyOpenSSL --upgrade
 sudo pip3 install cryptography --upgrade
 
 git submodule update --init --recursive
+
+./resetDatabase.sh
+popd
