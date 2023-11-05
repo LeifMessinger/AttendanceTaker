a66d92ca0a56242089c4f7eb0eb2204cc5e93d6d
Leif Messinger <leifmessinger@gmail.com>
2023-11-05 15:20:24 -0600

    Added install script

--- app/start.sh
+++ app/start.sh
@@ -1,3 +1,2 @@
 ./migrate.sh
-./manage.py collectstatic --noinput
-./manage.py runserver 0.0.0.0:8000
+python3 ./manage.py collectstatic --noinput && python3 ./manage.py runserver 0.0.0.0:8000
---
+++ installWSL.sh
@@ -0,0 +1,13 @@
+sudo apt-get install python3
+sudo apt-get install python-is-python3
+#probably reboot here
+pushd /tmp
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
+popd
+
+cd ./app
+pip3 install -r requirements.txt
+
+sudo pip3 install pyOpenSSL --upgrade
+sudo pip3 install cryptography --upgrade
