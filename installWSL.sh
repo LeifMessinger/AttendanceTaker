6f35ae6fd23006fd5f05841c019cb80db8c00ca3
Leif Messinger <leifmessinger@gmail.com>
2023-11-05 15:33:53 -0600

    Forgot to install wget

--- installWSL.sh
+++ installWSL.sh
@@ -2,6 +2,7 @@ sudo apt-get install python3
 sudo apt-get install python-is-python3
 #probably reboot here
 pushd /tmp
+sudo apt-get install wget
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
 popd
